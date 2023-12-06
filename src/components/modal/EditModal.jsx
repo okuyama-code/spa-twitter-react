6193d432efff9cbb869a8e4f1cd65cfa4b29a666
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-12-06 10:50:50 +0900

    profile_edit_modalのテンプレート完成

--- src/components/modal/EditModal.jsx
+++ src/components/modal/EditModal.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import "./Modal.scss";
+import { IoMdClose } from "react-icons/io";
+
+
 
 const EditModal = () => {
   return (
     <>
       <div className='edit_modal'>
-        <div>edit</div>
+        <div className='flex justify-between mt-1'>
+          <div className='flex'>
+            <IoMdClose className='close_icon' />
+            <h2 className='edit_h2'>Edit Profile</h2>
+          </div>
+
+          <input type="submit" value="保存する"  className='editButton'></input>
+          {/* <button  className='editButton'>保存する</button> */}
+        </div>
+
+        <div className='edit_profile_img'>
+          <img src="/assets/suisu0.jpg" alt="" className='header_img' />
+          <img src="/assets/person/icon.png" alt="" className='icon_img' />
+        </div>
+
+        <form className='edit_form'>
+          <input type="text" placeholder='お名前' />
+          <textarea name="" id="" cols="70" rows="4" placeholder='自己紹介文'></textarea>
+          <input type="text" placeholder='住んでいる場所' />
+          <input type="text" placeholder='ウェブサイト'  />
+          <input type="text" placeholder='1998/01/21' className='input_last' />
+        </form>
       </div>
     </>
   )
