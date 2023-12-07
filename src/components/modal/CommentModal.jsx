b8f02fc9ccb7bd6881d0e6e4af3e5d7a73f01a9a
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-12-07 18:03:08 +0900

    commentModalを少し作成

--- src/components/modal/CommentModal.jsx
+++ src/components/modal/CommentModal.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { IoMdClose } from 'react-icons/io'
 
 const CommentModal = () => {
   return (
-    <div className='comment_modal'>CommentModal</div>
+    <div className='comment_modal'>
+      <div className='comment_modal_header'>
+      {/* <button onClick={handleClick}> */}
+        <button>
+          <IoMdClose className='close_icon' />
+        </button>
+      </div>
+
+      <div className='comment_modal_post'>
+        <img src="/assets/person/icon.png" alt="" />
+        <div className='flex ml-3'>
+          <p>パク・ミニョン</p>
+          <p className='comment_modal_post_username'>@minyon01</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
