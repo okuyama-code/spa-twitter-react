d851e62a88eb8ec4bcb33ef5048588930e6e3397
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-12-08 05:31:15 +0900

    CommentModalのテンプレート完成

--- src/components/modal/CommentModal.jsx
+++ src/components/modal/CommentModal.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { IoMdClose } from 'react-icons/io'
+import { CiImageOn } from "react-icons/ci";
+
 
 const CommentModal = () => {
   return (
@@ -12,11 +14,29 @@ const CommentModal = () => {
       </div>
 
       <div className='comment_modal_post'>
-        <img src="/assets/person/icon.png" alt="" />
-        <div className='flex ml-3'>
-          <p>パク・ミニョン</p>
-          <p className='comment_modal_post_username'>@minyon01</p>
+        <img src="/assets/person/minyon.jpeg" alt="" />
+        <div className='ml-3'>
+          <div className='flex'>
+            <p>パク・ミニョン</p>
+            <p className='comment_modal_post_username'>@minyon01</p>
+          </div>
+          {/* TODO 投稿は改行が反映されるように */}
+          <p>返信したい投稿の内容です。難しいかもしれないが頑張ろう。<br />
+          返信したい投稿の内容です。難しいかもしれないが頑張ろう。
+          </p>
+          <p className='replying_to'>Replying to <span>@minyon01</span></p>
         </div>
+      </div>
+      <div className='comment_form'>
+        <img src="/assets/person/icon.png" alt="" />
+        <form>
+          <textarea name="" id="" cols="60" rows="5" placeholder='Post your reply'></textarea>
+          <div className='flex items-center justify-between mx-4'>
+            <CiImageOn className='img_icon'/>
+            <button>Reply</button>
+          </div>
+        </form>
+
       </div>
     </div>
   )
